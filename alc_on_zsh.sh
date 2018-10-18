d97c3b5c190e1cf5831b4bbe3b860e094f0e70b1
Takaya Saito <abcdefghijk_e_i_o@icloud.com>
2018-10-19 08:16:21 +0900

    minor change

--- alc_on_zsh.sh
+++ alc_on_zsh.sh
@@ -8,15 +8,16 @@ do
     "i" ) FLG_i="TRUE" ;;
     "h" ) FLG_h="TRUE" ;;
     "o" ) FLG_o="TRUE" ;;
-      * ) echo "Usage: $CMDNAME [-i] word" 1>&2
+      * ) echo "Usage: $CMDNAME [-h] [-i word] [-o word]" 1>&2
           exit 1;;
   esac
 done
 shift `expr $OPTIND - 1`
 if [[ "$FLG_h" = "TRUE" ]]; then
   echo "
-    Usage: $CMDNAME [-i] word
+    Usage: $CMDNAME [-h] [-i word] [-o word]
   " | sed  -e 's/^ *//g' -e '1d' -e '$d'
+  exit 0
 fi
 if [[ "$FLG_o" = "TRUE" ]]; then
   open 'http://eow.alc.co.jp/search?q='$1
@@ -26,7 +27,7 @@ fi
 red=`tput setaf 1`
 white=`tput setaf 7`
 reset=`tput sgr0`
-local NUM=`w3m -dump "http://eow.alc.co.jp/"$1 | grep -n 単語帳 | head -1 | awk -F ':' '{print $1}'`
+local NUM=`w3m -dump 'http://eow.alc.co.jp/search?q='$1 | grep -n 単語帳 | head -1 | awk -F ':' '{print $1}'`
 local NULL=""
 if [[ $NUM = $NULL ]]; then
   echo "${red}failed${reset}"
